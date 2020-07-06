ba3fe5fb2a7292bd1d6aab47e197cef95fe69fcb
vadimarsh <59313785+vadimarsh@users.noreply.github.com>
2020-07-06 21:03:27 +0800

    Initial commit

---
+++ app/src/main/java/com/example/androidarshinsky81/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.androidarshinsky81;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+
+
+
+}
